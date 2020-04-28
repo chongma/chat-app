715fb2508baa7334a0ba1c16f5c5bde6852a6d98
Matthew Broadhead <nbmlaw@gmail.com>
2020-04-28 15:59:29 +0200

    Initial commit

---
+++ src/utils/messages.js
@@ -0,0 +1,21 @@
+const generateMessage = (username, text) => {
+    return {
+        username,
+        text,
+        createdAt: new Date().getTime()
+    }
+}
+
+const generateLocationMessage = (username, { latitude, longitude }) => {
+    const url = `https://google.com/maps?q=${latitude},${longitude}`
+    return {
+        username,
+        url,
+        createdAt: new Date().getTime()
+    }
+}
+
+module.exports = {
+    generateMessage,
+    generateLocationMessage
+}
